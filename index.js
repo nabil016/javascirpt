0de232d4b8a5a760bc7d28e87c8bdc234ed1c818
nabil016 <153996513+nabil016@users.noreply.github.com>
2024-04-25 19:32:21 +0600

    website

---
+++ index.js
@@ -0,0 +1,25 @@
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+// let extraStudents = students % 3;
+
+
+
+// students += 3;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+students **;
+
+
+console.log(students);
